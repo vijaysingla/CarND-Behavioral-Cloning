fa23b498b86d38c19e411aaf7461d40dafe9ae96
vksingla <vksingla@umich.edu>
2017-11-10 00:21:23 -0500

    trained model on track 1

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 import cv2
 from sklearn.utils import shuffle
 import numpy as np 
-
+import matplotlib.image as mpimg
 def generator(samples, batch_size):
     num_samples = len(samples)
     while 1:
@@ -13,11 +13,41 @@ def generator(samples, batch_size):
             angles = []
 
             for list in batch_samples:
-                image_path = './data/'+ list[0]
-                centre_image = cv2.imread(image_path)
+                image_path = './data/IMG/'+ list[0].split('/')[-1]
+                #print(image_path)
+                left_image_path = './data/IMG/'+ list[1].split('/')[-1]
+                #print(left_image_path)
+                right_image_path = './data/IMG/'+ list[2].split('/')[-1]
+
+                centre_image = mpimg.imread(image_path)
+                #[:,:,::-1]
+                flip_image = np.fliplr(centre_image)
+
+                left_image =mpimg.imread(left_image_path)
+                #print(left_image)
+                right_image = mpimg.imread(right_image_path)
                 centre_angle = float(list[3])
+                flip_angle = -centre_angle
+                flip_left =  np.fliplr(left_image)
+                flip_right = np.fliplr (right_image)
+
+                steering_left = centre_angle + 0.2
+                steering_right = centre_angle - 0.2 
+                steering_flip_left = -steering_left
+                steering_flip_right = -steering_right
                 images.append(centre_image)
+                images.append(left_image)
+                images.append(right_image)
+                #images.append(flip_image)
+                #images.append(flip_left)
+                #images.append(flip_right)
                 angles.append(centre_angle)
+                angles.append(steering_left)
+                angles.append(steering_right)
+                #angles.append(flip_angle)
+                #angles.append(steering_flip_left)
+                #angles.append(steering_flip_right)
+
 
             X_train = np.array(images)
             Y_train = np.array(angles)
--- model.py
+++ model.py
@@ -6,10 +6,10 @@ import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Flatten , Dense , Activation, Lambda, Convolution2D, MaxPooling2D
+from keras.layers import Flatten , Dense , Activation, Lambda, Convolution2D, MaxPooling2D, Cropping2D
 from generator import generator
 import matplotlib.pyplot as plt
-
+plt.switch_backend('agg')
 print('Importing dependencies done')
 
 # Reading the images path from a csv file and using image path to read the image using cv2.imread 
@@ -47,42 +47,50 @@ with open(data_file,'r') as f:
         samples.append(row)
 samples = samples[1:] # skipping headers
 train_samples, validation_samples = train_test_split(samples, test_size = 0.2)
-BATCH_SIZE = 20 
-EPOCH = 5
+BATCH_SIZE = 64 
+EPOCH = 3
 
 train_generator = generator(train_samples, BATCH_SIZE)
 validation_generator = generator(validation_samples,BATCH_SIZE)
 
 # Neural Network architecture
 model = Sequential()
-model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape = image_shape))
-model.add(Convolution2D(32,3,3))
-model.add(MaxPooling2D((2,2)))
-model.add(Activation('relu'))
+model.add(Cropping2D(cropping =((70,25),(0,0)),input_shape = image_shape))
+model.add(Lambda(lambda x: (x/255.0)-0.5))
+#model.add(Lambda(lambda x: (x/255.0)-0.5,input_shape = image_shape))
+#model.add(Cropping2D(cropping = ((50,20),(0,0))))
+model.add(Convolution2D(24,5,2,subsample =(2,2),activation ='relu'))
+model.add(Convolution2D(36,5,2,subsample =(2,2),activation ='relu'))
+model.add(Convolution2D(48,5,2,subsample =(2,2),activation ='relu'))
+model.add(Convolution2D(64,3,1,activation ='relu'))
+model.add(Convolution2D(64,3,1,activation ='relu'))
 model.add(Flatten())
-model.add(Dense(32))
-model.add(Activation('relu'))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss ='mse' , optimizer = 'adam')
 
 #history_object = model.fit(_train , validation_split = 0.2, shuffle = True ,nb_epoch = EPOCH)
-history_object = model.fit_generator(train_generator, samples_per_epoch = len(train_samples), \
-    validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch = EPOCH, \
+history_object = model.fit_generator(train_generator, samples_per_epoch = 3*len(train_samples), \
+    validation_data = validation_generator, nb_val_samples = 3*len(validation_samples), nb_epoch = EPOCH, \
     verbose =1)
 ### print the keys contained in the history object
-#print(history_object.history.keys())
-
+print(history_object.history.keys())
 ### plot the training and validation loss for each epoch
-#plt.plot(history_object.history['loss'])
-#plt.plot(history_object.history['val_loss'])
-#plt.title('model mean squared error loss')
-#plt.ylabel('mean squared error loss')
-#plt.xlabel('epoch')
-#plt.legend(['training set', 'validation set'], loc='upper right')
+plt.figure(1)
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
 #plt.show()
+plt.savefig('./results.png')
 print('saving')
 model.save('model.h5')
 print('saved')
+
 
 
 
